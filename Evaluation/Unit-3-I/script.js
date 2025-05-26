fe57010e8ed4c1e5f9b35df293e986912b244144
punamh <kumari.punam8649@gmail.com>
2025-05-26 13:36:38 +0530

    Unit-3-I

---
+++ Evaluation/Unit-3-I/script.js
@@ -0,0 +1,33 @@
+let url ='https://unit-3-1-default-rtdb.firebaseio.com/students.json';
+
+async function displayData(){
+    let res = await fetch(url);
+    let data = await res.json();
+    console.log(data);
+
+    Object.values(data).forEach((ele)=>{
+        let box = document.getElementById("box");
+        let card = document.createElement("div");
+
+        let name = document.createElement("h2");
+        name.innerHTML ="Name :" + ele.name;
+
+        let age = document.createElement("h2");
+        age.innerHTML ="Age :" + ele.age;
+
+        let grade = document.createElement("h2");
+        grade.innerHTML ="Grade :" + ele.grade;
+
+        let score = document.createElement("h2");
+        score.innerHTML ="Score :" + ele.score;
+
+        let section = document.createElement("h2");
+        section.innerHTML ="Section :" + ele.section;
+
+        card.append(name,age,grade,score,section);
+        box.append(card);
+        
+    })
+}
+
+displayData()
